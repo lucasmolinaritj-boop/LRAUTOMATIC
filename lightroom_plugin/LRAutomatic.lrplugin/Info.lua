9bfdf6b8346c161dc94c6c8d4367048b0053cdd1
lucasmolinaritj-boop <lucasmolinari.tj@gmail.com>
2026-07-16 16:32:27 -0300

    Atualiza plugin para V3.2 Import Verified

--- lightroom_plugin/LRAutomatic.lrplugin/Info.lua
+++ lightroom_plugin/LRAutomatic.lrplugin/Info.lua
@@ -1,19 +1,19 @@
 return {
     LrSdkVersion = 6.0,
     LrSdkMinimumVersion = 6.0,
-    LrToolkitIdentifier = 'com.molinari.lrautomatic.v31',
-    LrPluginName = 'LRAutomatic V3.1 Task-Safe LR 10.4',
+    LrToolkitIdentifier = 'com.molinari.lrautomatic.v32',
+    LrPluginName = 'LRAutomatic V3.2 Import Verified LR 10.4',
     LrInitPlugin = 'Init.lua',
     LrShutdownPlugin = 'Shutdown.lua',
     LrLibraryMenuItems = {
         {
-            title = 'LRAutomatic V3.1 - Status e diagnóstico',
+            title = 'LRAutomatic V3.2 - Status e diagnóstico',
             file = 'RescueTest.lua',
         },
         {
-            title = 'LRAutomatic V3.1 - Processar fila agora',
+            title = 'LRAutomatic V3.2 - Processar fila agora',
             file = 'ProcessNow.lua',
         },
     },
-    VERSION = { major = 0, minor = 3, revision = 1, build = 104 },
+    VERSION = { major = 0, minor = 3, revision = 2, build = 104 },
 }
