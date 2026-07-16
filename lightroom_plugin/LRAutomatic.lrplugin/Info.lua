f8985963cb69e76f5fc62fe42592ee754c6727e0
lucasmolinaritj-boop <lucasmolinari.tj@gmail.com>
2026-07-16 15:17:18 -0300

    Bump plugin identity for V2.8 trace build

--- lightroom_plugin/LRAutomatic.lrplugin/Info.lua
+++ lightroom_plugin/LRAutomatic.lrplugin/Info.lua
@@ -1,19 +1,19 @@
 return {
     LrSdkVersion = 6.0,
     LrSdkMinimumVersion = 6.0,
-    LrToolkitIdentifier = 'com.molinari.lrautomatic.v26',
-    LrPluginName = 'LRAutomatic V2.6 LR 10.4 Hardened',
+    LrToolkitIdentifier = 'com.molinari.lrautomatic.v28',
+    LrPluginName = 'LRAutomatic V2.8 Trace LR 10.4',
     LrInitPlugin = 'Init.lua',
     LrShutdownPlugin = 'Shutdown.lua',
     LrLibraryMenuItems = {
         {
-            title = 'LRAutomatic V2.6 - Status e diagnóstico',
+            title = 'LRAutomatic V2.8 - Status e diagnóstico',
             file = 'RescueTest.lua',
         },
         {
-            title = 'LRAutomatic V2.6 - Processar fila agora',
+            title = 'LRAutomatic V2.8 - Processar fila agora',
             file = 'ProcessNow.lua',
         },
     },
-    VERSION = { major = 0, minor = 2, revision = 6, build = 104 },
+    VERSION = { major = 0, minor = 2, revision = 8, build = 104 },
 }
