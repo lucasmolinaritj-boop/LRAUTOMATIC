4cb05dc3d4f3212d929d75a898c7f77a6467179e
lucasmolinaritj-boop <lucasmolinari.tj@gmail.com>
2026-07-16 14:54:30 -0300

    Bump Lightroom plugin to hardened V2.6

--- lightroom_plugin/LRAutomatic.lrplugin/Info.lua
+++ lightroom_plugin/LRAutomatic.lrplugin/Info.lua
@@ -1,19 +1,19 @@
 return {
     LrSdkVersion = 6.0,
     LrSdkMinimumVersion = 6.0,
-    LrToolkitIdentifier = 'com.molinari.lrautomatic.v24',
-    LrPluginName = 'LRAutomatic V2.4 Compatibilidade LR 10.4',
+    LrToolkitIdentifier = 'com.molinari.lrautomatic.v26',
+    LrPluginName = 'LRAutomatic V2.6 LR 10.4 Hardened',
     LrInitPlugin = 'Init.lua',
     LrShutdownPlugin = 'Shutdown.lua',
     LrLibraryMenuItems = {
         {
-            title = 'LRAutomatic V2.4 - Status e diagnóstico',
+            title = 'LRAutomatic V2.6 - Status e diagnóstico',
             file = 'RescueTest.lua',
         },
         {
-            title = 'LRAutomatic V2.4 - Processar fila agora',
+            title = 'LRAutomatic V2.6 - Processar fila agora',
             file = 'ProcessNow.lua',
         },
     },
-    VERSION = { major = 0, minor = 2, revision = 4, build = 104 },
+    VERSION = { major = 0, minor = 2, revision = 6, build = 104 },
 }
