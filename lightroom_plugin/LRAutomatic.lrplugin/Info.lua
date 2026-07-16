30a8b246c6881f82a3634bd2472c91ce5a40478d
lucasmolinaritj-boop <lucasmolinari.tj@gmail.com>
2026-07-16 16:54:20 -0300

    chore: publish V3.4 single-loop build

--- lightroom_plugin/LRAutomatic.lrplugin/Info.lua
+++ lightroom_plugin/LRAutomatic.lrplugin/Info.lua
@@ -1,19 +1,19 @@
 return {
     LrSdkVersion = 6.0,
     LrSdkMinimumVersion = 6.0,
-    LrToolkitIdentifier = 'com.molinari.lrautomatic.v33',
-    LrPluginName = 'LRAutomatic V3.3 Write Wait LR 10.4',
+    LrToolkitIdentifier = 'com.molinari.lrautomatic.v34',
+    LrPluginName = 'LRAutomatic V3.4 Single Loop LR 10.4',
     LrInitPlugin = 'Init.lua',
     LrShutdownPlugin = 'Shutdown.lua',
     LrLibraryMenuItems = {
         {
-            title = 'LRAutomatic V3.3 - Status e diagnóstico',
+            title = 'LRAutomatic V3.4 - Status e diagnóstico',
             file = 'RescueTest.lua',
         },
         {
-            title = 'LRAutomatic V3.3 - Processar fila agora',
+            title = 'LRAutomatic V3.4 - Processar fila agora',
             file = 'ProcessNow.lua',
         },
     },
-    VERSION = { major = 0, minor = 3, revision = 3, build = 104 },
+    VERSION = { major = 0, minor = 3, revision = 4, build = 104 },
 }
