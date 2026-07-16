febe87fe60b0d50416963e03f9efbed44dd7ba3b
lucasmolinaritj-boop <lucasmolinari.tj@gmail.com>
2026-07-16 16:41:44 -0300

    chore: bump plugin to v3.3 write wait

--- lightroom_plugin/LRAutomatic.lrplugin/Info.lua
+++ lightroom_plugin/LRAutomatic.lrplugin/Info.lua
@@ -1,19 +1,19 @@
 return {
     LrSdkVersion = 6.0,
     LrSdkMinimumVersion = 6.0,
-    LrToolkitIdentifier = 'com.molinari.lrautomatic.v32',
-    LrPluginName = 'LRAutomatic V3.2 Import Verified LR 10.4',
+    LrToolkitIdentifier = 'com.molinari.lrautomatic.v33',
+    LrPluginName = 'LRAutomatic V3.3 Write Wait LR 10.4',
     LrInitPlugin = 'Init.lua',
     LrShutdownPlugin = 'Shutdown.lua',
     LrLibraryMenuItems = {
         {
-            title = 'LRAutomatic V3.2 - Status e diagnóstico',
+            title = 'LRAutomatic V3.3 - Status e diagnóstico',
             file = 'RescueTest.lua',
         },
         {
-            title = 'LRAutomatic V3.2 - Processar fila agora',
+            title = 'LRAutomatic V3.3 - Processar fila agora',
             file = 'ProcessNow.lua',
         },
     },
-    VERSION = { major = 0, minor = 3, revision = 2, build = 104 },
+    VERSION = { major = 0, minor = 3, revision = 3, build = 104 },
 }
