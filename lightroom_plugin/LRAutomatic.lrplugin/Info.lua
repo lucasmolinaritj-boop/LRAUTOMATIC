c78637cc43e7f21ecce733c0ef127af2b9113548
lucasmolinaritj-boop <lucasmolinari.tj@gmail.com>
2026-07-16 17:24:34 -0300

    brand v4 pipeline and enable plugin manager panel

--- lightroom_plugin/LRAutomatic.lrplugin/Info.lua
+++ lightroom_plugin/LRAutomatic.lrplugin/Info.lua
@@ -1,19 +1,20 @@
 return {
     LrSdkVersion = 6.0,
     LrSdkMinimumVersion = 6.0,
-    LrToolkitIdentifier = 'com.molinari.lrautomatic.v34',
-    LrPluginName = 'LRAutomatic V3.4 Single Loop LR 10.4',
+    LrToolkitIdentifier = 'com.molinari.lrautomatic.v40',
+    LrPluginName = 'LRAutomatic V4.0 Catálogo + Preset + Smart Preview',
     LrInitPlugin = 'Init.lua',
     LrShutdownPlugin = 'Shutdown.lua',
+    LrPluginInfoProvider = 'PluginInfoProvider.lua',
     LrLibraryMenuItems = {
         {
-            title = 'LRAutomatic V3.4 - Status e diagnóstico',
+            title = 'LRAutomatic V4.0 - Painel e diagnóstico',
             file = 'RescueTest.lua',
         },
         {
-            title = 'LRAutomatic V3.4 - Processar fila agora',
+            title = 'LRAutomatic V4.0 - Processar fila agora',
             file = 'ProcessNow.lua',
         },
     },
-    VERSION = { major = 0, minor = 3, revision = 4, build = 104 },
+    VERSION = { major = 0, minor = 4, revision = 0, build = 104 },
 }
