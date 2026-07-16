9a87d954e76dc017f00f9571177ae0b1374b838f
lucasmolinaritj-boop <lucasmolinari.tj@gmail.com>
2026-07-16 15:51:28 -0300

    Bump plugin to V2.9 native file IO

--- lightroom_plugin/LRAutomatic.lrplugin/Info.lua
+++ lightroom_plugin/LRAutomatic.lrplugin/Info.lua
@@ -1,19 +1,19 @@
 return {
     LrSdkVersion = 6.0,
     LrSdkMinimumVersion = 6.0,
-    LrToolkitIdentifier = 'com.molinari.lrautomatic.v28',
-    LrPluginName = 'LRAutomatic V2.8 Trace LR 10.4',
+    LrToolkitIdentifier = 'com.molinari.lrautomatic.v29',
+    LrPluginName = 'LRAutomatic V2.9 Native IO LR 10.4',
     LrInitPlugin = 'Init.lua',
     LrShutdownPlugin = 'Shutdown.lua',
     LrLibraryMenuItems = {
         {
-            title = 'LRAutomatic V2.8 - Status e diagnóstico',
+            title = 'LRAutomatic V2.9 - Status e diagnóstico',
             file = 'RescueTest.lua',
         },
         {
-            title = 'LRAutomatic V2.8 - Processar fila agora',
+            title = 'LRAutomatic V2.9 - Processar fila agora',
             file = 'ProcessNow.lua',
         },
     },
-    VERSION = { major = 0, minor = 2, revision = 8, build = 104 },
+    VERSION = { major = 0, minor = 2, revision = 9, build = 104 },
 }
