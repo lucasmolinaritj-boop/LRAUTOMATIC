c1c1f9b2605a0026c5ca258a9c4c6f78deb8005a
lucasmolinaritj-boop <lucasmolinari.tj@gmail.com>
2026-07-16 12:01:55 -0300

    Compatibiliza plugin com Lightroom Classic 10.4

--- lightroom_plugin/LRAutomatic.lrplugin/Info.lua
+++ lightroom_plugin/LRAutomatic.lrplugin/Info.lua
@@ -2,9 +2,20 @@ return {
     LrSdkVersion = 10.0,
     LrSdkMinimumVersion = 6.0,
     LrToolkitIdentifier = 'com.molinari.lrautomatic',
-    LrPluginName = 'LRAutomatic',
+    LrPluginName = 'LRAutomatic 10.4',
     LrPluginInfoUrl = 'https://github.com/lucasmolinaritj-boop/LRAUTOMATIC',
     LrInitPlugin = 'Init.lua',
     LrShutdownPlugin = 'Shutdown.lua',
-    VERSION = { major = 0, minor = 1, revision = 0, build = 1 },
+    LrPluginInfoProvider = 'PluginInfoProvider.lua',
+    LrLibraryMenuItems = {
+        {
+            title = 'LRAutomatic - Processar fila agora',
+            file = 'ProcessNow.lua',
+        },
+        {
+            title = 'LRAutomatic - Testar plugin',
+            file = 'TestPlugin.lua',
+        },
+    },
+    VERSION = { major = 0, minor = 2, revision = 0, build = 104 },
 }
