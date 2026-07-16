f15d713afa27730d6c21acfbe3923b679335068a
lucasmolinaritj-boop <lucasmolinari.tj@gmail.com>
2026-07-16 13:01:06 -0300

    Bump instrumented plugin path-fix version

--- lightroom_plugin/LRAutomatic.lrplugin/Info.lua
+++ lightroom_plugin/LRAutomatic.lrplugin/Info.lua
@@ -2,20 +2,20 @@ return {
     LrSdkVersion = 10.0,
     LrSdkMinimumVersion = 6.0,
     LrToolkitIdentifier = 'com.molinari.lrautomatic',
-    LrPluginName = 'LRAutomatic V2 Instrumentado',
+    LrPluginName = 'LRAutomatic V2.2 Instrumentado',
     LrPluginInfoUrl = 'https://github.com/lucasmolinaritj-boop/LRAUTOMATIC',
     LrInitPlugin = 'Init.lua',
     LrShutdownPlugin = 'Shutdown.lua',
     LrPluginInfoProvider = 'PluginInfoProvider.lua',
     LrLibraryMenuItems = {
         {
-            title = 'LRAutomatic V2 - Processar fila agora',
+            title = 'LRAutomatic V2.2 - Processar fila agora',
             file = 'ProcessNow.lua',
         },
         {
-            title = 'LRAutomatic V2 - Teste instrumentado',
+            title = 'LRAutomatic V2.2 - Teste instrumentado',
             file = 'TestPlugin.lua',
         },
     },
-    VERSION = { major = 0, minor = 2, revision = 1, build = 104 },
+    VERSION = { major = 0, minor = 2, revision = 2, build = 104 },
 }
