a775d428450534c52b216bc2a7d903baecb369bb
lucasmolinaritj-boop <lucasmolinari.tj@gmail.com>
2026-07-16 12:46:11 -0300

    mark instrumented plugin v2.1

--- lightroom_plugin/LRAutomatic.lrplugin/Info.lua
+++ lightroom_plugin/LRAutomatic.lrplugin/Info.lua
@@ -2,20 +2,20 @@ return {
     LrSdkVersion = 10.0,
     LrSdkMinimumVersion = 6.0,
     LrToolkitIdentifier = 'com.molinari.lrautomatic',
-    LrPluginName = 'LRAutomatic 10.4',
+    LrPluginName = 'LRAutomatic V2 Instrumentado',
     LrPluginInfoUrl = 'https://github.com/lucasmolinaritj-boop/LRAUTOMATIC',
     LrInitPlugin = 'Init.lua',
     LrShutdownPlugin = 'Shutdown.lua',
     LrPluginInfoProvider = 'PluginInfoProvider.lua',
     LrLibraryMenuItems = {
         {
-            title = 'LRAutomatic - Processar fila agora',
+            title = 'LRAutomatic V2 - Processar fila agora',
             file = 'ProcessNow.lua',
         },
         {
-            title = 'LRAutomatic - Testar plugin',
+            title = 'LRAutomatic V2 - Teste instrumentado',
             file = 'TestPlugin.lua',
         },
     },
-    VERSION = { major = 0, minor = 2, revision = 0, build = 104 },
+    VERSION = { major = 0, minor = 2, revision = 1, build = 104 },
 }
