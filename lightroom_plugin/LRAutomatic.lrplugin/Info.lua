65d4135291e6c2af833959c1c377e51b6a3c0c60
lucasmolinaritj-boop <lucasmolinari.tj@gmail.com>
2026-07-16 11:13:50 -0300

    Add Lightroom plugin manifest

---
+++ lightroom_plugin/LRAutomatic.lrplugin/Info.lua
@@ -0,0 +1,10 @@
+return {
+    LrSdkVersion = 10.0,
+    LrSdkMinimumVersion = 6.0,
+    LrToolkitIdentifier = 'com.molinari.lrautomatic',
+    LrPluginName = 'LRAutomatic',
+    LrPluginInfoUrl = 'https://github.com/lucasmolinaritj-boop/LRAUTOMATIC',
+    LrInitPlugin = 'Init.lua',
+    LrShutdownPlugin = 'Shutdown.lua',
+    VERSION = { major = 0, minor = 1, revision = 0, build = 1 },
+}
